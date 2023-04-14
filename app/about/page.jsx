68bb477a2e50416986cff3d242aa8f123240a32b
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2023-04-14 14:09:29 -0500

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <div style={{color:'white'}}>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, sit? Obcaecati numquam molestias eligendi quis deleniti nam cupiditate rem delectus commodi porro vel iure, expedita voluptatibus nesciunt itaque eos, aliquid recusandae ipsa suscipit nemo alias error quasi rerum sint? Libero autem voluptatibus inventore obcaecati molestias laudantium blanditiis quod illo. Corrupti expedita placeat, esse ratione facere repudiandae atque distinctio voluptates perspiciatis cumque repellendus veniam reprehenderit laudantium doloremque harum. Nesciunt perspiciatis doloremque iure sapiente. Vitae repellat assumenda nihil cumque possimus aut a tenetur nemo, modi laboriosam, dolore enim deserunt repellendus mollitia quos sunt est. Amet corrupti atque deserunt veniam quia. Maxime, atque.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function ServicesPage() {
+  return (
+    <div style={{color:'white'}}>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae tempore qui veniam, repudiandae eius aliquid itaque, nam velit deserunt nobis sed architecto beatae rem quae voluptates incidunt accusantium. Maxime provident in, ipsam quas cum ratione, amet similique maiores vitae illo, error quibusdam rerum vero id! Alias libero nulla rerum, praesentium debitis ipsum voluptas consequatur blanditiis dicta, enim eligendi illum quia sint magnam repellendus eos illo ducimus voluptatum impedit suscipit neque nesciunt provident. Consequatur, fuga voluptatum amet dignissimos ullam repellat, numquam sit omnis quos earum unde? Eligendi sed possimus obcaecati? Eos ipsum vero aliquid consequuntur impedit ipsa, doloremque voluptatum voluptates quod.</p>
+    </div>
+  )
+}
+
+export default ServicesPage
